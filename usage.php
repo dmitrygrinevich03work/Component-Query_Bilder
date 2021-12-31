a1534f53a7d37be66ab56df76723d40f335fbe67
dmitrygrinevich03work <dmitrygrinevich03work@gmail.com>
2021-12-31 20:47:06 +0300

    Update usage.php

--- usage.php
+++ usage.php
@@ -1,13 +1,24 @@
 <?php
+
 /* Connect component QueryBilder */
-include "Component/QueryBilder.php";//We connect the component
+require_once "Component/QueryBilder.php";//We connect the component
 
 $connect_db = new PDO("mysql:host=localhost; dbname=level; charset=utf8;" , "root", "");//DB connection
+
 $db = new QueryBilder($connect_db);//Create a QueryBuilder object and pass it a database connection
 
 
-$db->getAll('posts');//We call the function to load all records from the database
-$db->getOne('posts', 1);//Get one record from a table
-$db->create('posts', ['title' => 'text random']);//Create record in table
-$db->update('posts', ['title' => 'text random'], 2);//Update a record in a table
-$db->delete('posts', 2);//Delete a record in a table
+/* We call the function to load all records from the database */
+$db->getAll('posts');
+
+/* Get one record from a table */
+$db->getOne('posts', 1);
+
+/* Create record in table */
+$db->create('posts', ['title' => 'text random']);
+
+/* Update a record in a table */
+$db->update('posts', ['title' => 'text random'], 2);
+
+/* Delete a record in a table */
+$db->delete('posts', 2);
